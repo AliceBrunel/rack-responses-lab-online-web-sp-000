6404e4f9872c1a6ec089045e030571284f0a287f
Alice Brunel <alc.brunel@gmail.com>
2019-09-21 17:39:24 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -2,8 +2,11 @@ class Application
  
   def call(env)
     resp = Rack::Response.new
-    time_now = Time.new
-    resp.write "Hello, World"
+    time_now = Time.now.hour < 12 ? 'morning' : 'afternoon'
+    if time_now = 'morning'
+      resp.write "Good Morning!"
+    else 
+      resp.write "Good Afternoon!"
     resp.finish
   end
  
